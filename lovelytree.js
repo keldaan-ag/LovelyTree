d0415404a088b8d3bd8a2d31cf4d95de217300c4
Arnaud Grégoire <arnaudgregoire92@gmail.com>
2017-06-07 14:17:37 +0200

    Amélioration significative de la beauté du bouzin

--- lovelytree.js
+++ lovelytree.js
@@ -5,30 +5,45 @@ canvas.height = 1000;
 var ctx    = canvas.getContext("2d");
 
 
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
 ctx.beginPath();
 ctx.lineCap="round";
 ctx.lineJoin="round";
-ctx.strokeStyle="brown";
+ctx.strokeStyle="#94716b";
+ctx.lineWidth = 10;
+color = getRandomColor();
+
 
 
 function branche(gen,x,y){
 	
-	if(gen<5){
+	if(gen<8){
 		
 		ctx.moveTo(0,0);
-		ctx.lineTo(0,-y/10);	
-		ctx.lineWidth = 15/gen;
+		ctx.lineTo(0,-y/7);	
 		ctx.stroke();
-		ctx.translate(0,-y/10);
-
-		ctx.rotate(-Math.random()*0.1+0.1)
-		if(Math.random()<0.8){
+		ctx.translate(0,-y/7);
+		ctx.rotate(Math.random()*0.2-0.1)
+		
+		if(Math.random()<0.6){
 						
-			ctx.rotate(-0.3);		
+			ctx.rotate(-0.3);	
+			ctx.scale(0.6,0.6);		
+			
 			ctx.save();
 			branche(gen+1,x,y);
 			ctx.restore();			
+			
 			ctx.rotate(0.6);
+			
 			ctx.save();
 			branche(gen+1,x,y);
 			ctx.restore();
@@ -39,10 +54,10 @@ function branche(gen,x,y){
 		}	
 	}
 	else{
-		ctx.fillStyle = 'lightgreen';
-		ctx.fillRect(0,0,50,50); 
+		ctx.fillStyle = color ;
+		ctx.fillRect(0,0,2000,2000); 
 	}
 }
 ctx.translate(canvas.width/2,canvas.height)
-branche(0,canvas.width/2,canvas.height,15)
+branche(0,canvas.width/2,canvas.height)
 
