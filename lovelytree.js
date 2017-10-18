ccd2c3caed7064b3cf45f4dd32e8852ca73d8dae
arnaudgregoire <arnaud.gregoire@ensg.eu>
2017-10-18 22:55:32 +0200

    La doc

--- lovelytree.js
+++ lovelytree.js
@@ -1,3 +1,11 @@
+/*
+Génération aléatoire récursive de jolis arbres dans un canvas
+*/
+
+
+/*
+On assure la correspondance entre HTML et js en récupérant l'objet canvas et son contexte
+*/
 var canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
@@ -8,7 +16,9 @@ ctx.strokeStyle="#94716b";
 ctx.lineWidth = 7;
 color = getRandomColor();
 
-
+/*
+getRandomColor permet de créér une couleur aléatoire
+*/
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
@@ -18,6 +28,9 @@ function getRandomColor() {
 	return color;
 }
 
+/*
+On dessine un arbre en dessinant des branches
+*/
 function arbre(x,y){
 	ctx.save();
 	color = getRandomColor();
@@ -27,9 +40,17 @@ function arbre(x,y){
 	
 }
 
+/*
+fonction principale, implémentant la récursivité de l'algorithme
+*/
 function branche(gen,x,y,step){
 	var l_branche = 0;
-	if(step<8){
+	
+	if(step<8){ // Condition d'arret 
+	
+		/*
+		On dessine la branche en décidant de son inclinaison et de sa longueur
+		*/
 		ctx.beginPath();
 		ctx.moveTo(0,0);
 		l_branche = -y/7 - (y/7)* (Math.random()*2-1)
@@ -38,6 +59,10 @@ function branche(gen,x,y,step){
 		ctx.translate(0,l_branche);
 		ctx.rotate(Math.random()*0.2-0.1)
 		
+		
+		/*
+		Chaque branche peut se diviser et donner naissance à 2 autres branches
+		*/
 		if(Math.random()<0.6){
 						
 			ctx.rotate(-0.3);	
@@ -53,6 +78,10 @@ function branche(gen,x,y,step){
 			branche(gen+1,x,y,step+1);
 			ctx.restore();
 		}
+		
+		/*
+		Si elle ne se divise pas, elle continue avec une autre branche
+		*/
 		else{
 			ctx.scale(0.8,0.8);	
 			ctx.save();
@@ -61,6 +90,9 @@ function branche(gen,x,y,step){
 		}	
 		ctx.closePath();
 	}
+	/*
+	Quand toutes les branches ont été généré, on dessine les feuilles
+	*/
 	else{
 		ctx.beginPath();
 		ctx.fillStyle   = color ;
@@ -72,9 +104,7 @@ function branche(gen,x,y,step){
 	}
 }
 /*
-arbre(canvas.width/4, canvas.height)
-arbre(3 * canvas.width/4, canvas.height)
-arbre(2 * canvas.width/4, canvas.height)
+On veut dessiner 2 arbres
 */
 
 for (var i = 1; i < 3; i++ ) {
