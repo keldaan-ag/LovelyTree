6e260002cb090a4d78ca74de009d5760ef605a10
arnaudgregoire <arnaud.gregoire@ensg.eu>
2018-02-08 09:47:56 +0100

    dat.gui

--- README.md
+++
@@ -1,5 +0,0 @@
-# LovelyTree
-Génération aléatoire récursive de jolis arbres  dans un canvas js
- ![des jolis arbres](img/img2.PNG)
-Un autre exemple ![des jolis arbres](img/img.PNG)
-et encore un autre ![des jolis arbres](img/img3.PNG)
--- lovelytree.js
+++ lovelytree.js
@@ -1,11 +1,3 @@
-/*
-Génération aléatoire récursive de jolis arbres dans un canvas
-*/
-
-
-/*
-On assure la correspondance entre HTML et js en récupérant l'objet canvas et son contexte
-*/
 var canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
@@ -15,10 +7,38 @@ ctx.lineJoin="round";
 ctx.strokeStyle="#94716b";
 ctx.lineWidth = 7;
 color = getRandomColor();
+var r = 360;
 
-/*
-getRandomColor permet de créér une couleur aléatoire
-*/
+var FizzyText = function() {
+  this.description = 'Lovely trees';
+  this.number           = 3;
+  this.remove_last_Tree = false;
+  this.rayon            = 260;
+
+  this.create = function() {
+  	r = this.rayon +100;
+  	if (this.remove_last_Tree) {
+  		ctx.clearRect(0, 0, canvas.width, canvas.height);
+  	}
+
+	for (var i = 1; i < this.number+1; i++ ) {
+		arbre(i*canvas.width/(this.number+1),canvas.height)
+	}
+  };
+};
+
+window.onload = function() {
+  var text = new FizzyText();
+  var gui = new dat.GUI();
+  gui.add(text, 'description');
+  gui.add(text, 'number', 0, 1000, 1);
+  gui.add(text, 'rayon', 0, 1000, 1);
+  gui.add(text, 'remove_last_Tree');
+  gui.add(text, 'create');
+};
+    
+
+
 function getRandomColor() {
 	var letters = '0123456789ABCDEF';
 	var color = '#';
@@ -28,9 +48,6 @@ function getRandomColor() {
 	return color;
 }
 
-/*
-On dessine un arbre en dessinant des branches
-*/
 function arbre(x,y){
 	ctx.save();
 	color = getRandomColor();
@@ -40,75 +57,70 @@ function arbre(x,y){
 	
 }
 
-/*
-fonction principale, implémentant la récursivité de l'algorithme
-*/
 function branche(gen,x,y,step){
 	var l_branche = 0;
-	
-	if(step<8){ // Condition d'arret 
-	
-		/*
-		On dessine la branche en décidant de son inclinaison et de sa longueur
-		*/
-		ctx.beginPath();
-		ctx.moveTo(0,0);
-		l_branche = -y/7 - (y/7)* (Math.random()*2-1)
-		ctx.lineTo(0,l_branche);	
-		ctx.stroke();
-		ctx.translate(0,l_branche);
-		ctx.rotate(Math.random()*0.2-0.1)
-		
-		
-		/*
-		Chaque branche peut se diviser et donner naissance à 2 autres branches
-		*/
+	if(step<8){
+		draw(x,y);
+
 		if(Math.random()<0.6){
 						
 			ctx.rotate(-0.3);	
 			ctx.scale(0.8,0.8);		
 			
 			ctx.save();
-			branche(gen+1,x,y,step+1);
+			//wait(500);
+			branche(gen+1,x,y,step+1)
+			//setTimeout(()=>{ branche(gen+1,x,y,step+1) }, 500);
+			
 			ctx.restore();			
 			
 			ctx.rotate(0.6);
 			
 			ctx.save();
-			branche(gen+1,x,y,step+1);
+			//wait(500);
+			branche(gen+1,x,y,step+1)
+			//setTimeout(()=>{ branche(gen+1,x,y,step+1) }, 500);
 			ctx.restore();
 		}
-		
-		/*
-		Si elle ne se divise pas, elle continue avec une autre branche
-		*/
 		else{
 			ctx.scale(0.8,0.8);	
 			ctx.save();
-			branche(gen,x,y,step+1);
+			//wait(500);
+			branche(gen,x,y,step+1)
+			//setTimeout(()=>{ branche(gen,x,y,step+1) }, 500);
 			ctx.restore();	
 		}	
-		ctx.closePath();
+
 	}
-	/*
-	Quand toutes les branches ont été généré, on dessine les feuilles
-	*/
 	else{
 		ctx.beginPath();
 		ctx.fillStyle   = color ;
 		ctx.strokeStyle = color;
-		ctx.arc(0, 0, 360 - step*10 + Math.random() * 50, 0, 2 * Math.PI, false);
+		ctx.arc(0, 0, r - step*10 + Math.random() * 50, 0, 2 * Math.PI, false);
 		ctx.fill();
 		ctx.stroke();
 		ctx.closePath();
 	}
 }
-/*
-On veut dessiner 2 arbres
-*/
 
-for (var i = 1; i < 3; i++ ) {
-	arbre(i*canvas.width/3,canvas.height)
+function draw(x,y) {
+	//console.log("test");
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	let l_branche = -y/7 - (y/7)* (Math.random()*2-1)
+	ctx.lineTo(0,l_branche);	
+	ctx.stroke();
+	ctx.translate(0,l_branche);
+	ctx.rotate(Math.random()*0.2-0.1)
+	ctx.closePath();
 }
+
+
+
+arbre(canvas.width/4, canvas.height)
+arbre(3 * canvas.width/4, canvas.height)
+arbre(2 * canvas.width/4, canvas.height)
+
+
 
 
